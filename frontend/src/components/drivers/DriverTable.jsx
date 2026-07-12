e9d8df50430515c83702c00567804a6bb6da8885
Shrey <shresthabhoi012@gmail.com>
2026-07-12 11:17:04 +0530

    feat: add driver management UI componenets

---
+++ frontend/src/components/drivers/DriverTable.jsx
@@ -0,0 +1,63 @@
+export default function DriverTable() {
+  const drivers = [
+    {
+      id: 1,
+      name: "Rahul Sharma",
+      vehicle: "Truck A",
+      status: "On Duty",
+      phone: "+91 9876543210",
+    },
+    {
+      id: 2,
+      name: "Priya Singh",
+      vehicle: "Truck B",
+      status: "Off Duty",
+      phone: "+91 9123456789",
+    },
+    {
+      id: 3,
+      name: "Amit Kumar",
+      vehicle: "Truck C",
+      status: "On Duty",
+      phone: "+91 9988776655",
+    },
+  ];
+
+  return (
+    <div
+      style={{
+        background: "#1f2937",
+        borderRadius: "12px",
+        padding: "20px",
+        color: "white",
+      }}
+    >
+      <table
+        style={{
+          width: "100%",
+          borderCollapse: "collapse",
+        }}
+      >
+        <thead>
+          <tr>
+            <th align="left">Driver</th>
+            <th align="left">Vehicle</th>
+            <th align="left">Status</th>
+            <th align="left">Phone</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {drivers.map((driver) => (
+            <tr key={driver.id}>
+              <td>{driver.name}</td>
+              <td>{driver.vehicle}</td>
+              <td>{driver.status}</td>
+              <td>{driver.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- frontend/src/pages/Drivers.jsx
+++ frontend/src/pages/Drivers.jsx
@@ -1,9 +1,35 @@
 import Layout from "../components/layout/Layout";
+import DriverSearch from "../components/drivers/DriverSearch";
+import AddDriverButton from "../components/drivers/AddDriverButton";
+import DriverTable from "../components/drivers/DriverTable";
 
 export default function Drivers() {
   return (
     <Layout>
-      <h1>Drivers</h1>
+      <div
+        style={{
+          padding: "30px",
+          maxWidth: "1200px",
+          margin: "0 auto",
+        }}
+      >
+        <h1>Drivers</h1>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginTop: "24px",
+            marginBottom: "24px",
+          }}
+        >
+          <DriverSearch />
+          <AddDriverButton />
+        </div>
+
+        <DriverTable />
+      </div>
     </Layout>
   );
 }
